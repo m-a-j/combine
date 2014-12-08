439f59171380e4f60d19ac9776d2ec86205f181a
Markus <markus.jenning@web.de>
2014-12-08 13:33:25 +0100

    removed magrittr syntax
    changed from magrittr to base syntax to allow specific item selection,
    added if-not-already-exists condition

--- goscr.R
+++ goscr.R
@@ -1,14 +1,14 @@
 library("rvest")
 library("magrittr")
 
-mainDir <- "C:/Users/Markus/Pictures/gocomics/"
-# 
-subDir <- "thebarn"
+mainDir <- "C:/Users/Markus/Pictures/gocomics"
+# subDir: cartoon name == folder name
+subDir <- "peanuts"
 
 dir.create(file.path(mainDir, subDir))
 setwd(file.path(mainDir, subDir))
 
-for(yy in 2011:2011){
+for(yy in 1974:1974){
   yys <- sprintf("%02d", yy)
   for(mm in 1:12){
     mms <- sprintf("%02d", mm)
@@ -17,20 +17,24 @@ for(yy in 2011:2011){
       if(!((mm==2||mm==4||mm==6||mm==9||mm==11)&&(dd==31))) {
         
         dds <- sprintf("%02d", dd)
-        
-        pgurl <- paste("http://www.gocomics.com/", subDir, "/",
-                       yys, "/", mms, "/", dds, sep = "")
-        src <- html(pgurl) %>%
-                 html_nodes("img.strip") %>%
-                 .[[1]] %>%
-                 html_attr("src")
-  
         dest <- paste(subDir, yys, mms, dds, ".png", sep="")
         
-        message(dest)
+        if(!file.exists(dest)) {
+          
+          pgurl <- paste("http://www.gocomics.com/", subDir, "/",
+                         yys, "/", mms, "/", dds, sep = "")
+          pghtml <- html(pgurl)
+          nodes <- html_nodes(x = pghtml, "img.strip")
+          # choose the last link (to highest resolution image)
+          imgsrc <- html_attr(x = nodes[[length(nodes)]], "src")
+
+          message(paste("Downloading ", dest, "...", sep=""))
+          download.file(imgsrc, destfile=dest, mode="wb", quiet = T)
+          #rstudio::viewer(url = dest)
         
-        download.file(src, destfile=dest, mode="wb")
-        
+        } else {
+          message(paste(dest,"already exists"))
+        }        
       }
     }
   }
